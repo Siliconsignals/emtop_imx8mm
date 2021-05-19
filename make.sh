f0ceb09258655c2b72979a8e7996958acbe263a0
Jeffery.Cheng <gitusr@tech.com>
2021-05-19 12:06:59 +0800

    Add: fsl-imx8mm-demo-hdmi.dts, fix: make.sh

--- make.sh
+++ make.sh
@@ -9,8 +9,10 @@ DESTDIR="/dev/shm/"
 build_imx8mm() {
 	SRCDTB1="arch/arm64/boot/dts/freescale/fsl-imx8mm-demo.dtb"
 	DSTDTB1="fsl-imx8mm-demo.dtb"
-	SRCDTB2="arch/arm64/boot/dts/freescale/fsl-imx8mm-demo-no-pcie.dtb"
-	DSTDTB2="fsl-imx8mm-demo-no-pcie.dtb"
+	SRCDTB2="arch/arm64/boot/dts/freescale/fsl-imx8mm-demo-hdmi.dtb"
+	DSTDTB2="fsl-imx8mm-demo-hdmi.dtb"
+	SRCDTB3="arch/arm64/boot/dts/freescale/fsl-imx8mm-demo-no-pcie.dtb"
+	DSTDTB3="fsl-imx8mm-demo-no-pcie.dtb"
 	SRCKER="arch/arm64/boot/Image"
 	DSTKER="Image"
 
@@ -34,6 +36,8 @@ build_imx8mm() {
 		cp -f ${SRCDTB1} ${d}/${DSTDTB1}
 		echo "Info: COPY ${SRCDTB2} -> ${d}/${DSTDTB2}"
 		cp -f ${SRCDTB2} ${d}/${DSTDTB2}
+		echo "Info: COPY ${SRCDTB3} -> ${d}/${DSTDTB3}"
+		cp -f ${SRCDTB3} ${d}/${DSTDTB3}
 		echo "Info: COPY ${SRCKER} ->  ${d}/${DSTKER}"
 		cp -f ${SRCKER} ${d}/${DSTKER}
 	done
